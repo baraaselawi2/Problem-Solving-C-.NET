d0c31177dd4166ef4627cda8422961d0321be426
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-07 14:12:21 +0300

    Add files via upload

---
+++ Receipt Total Calculator/Receipt Total Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Numerics;
+
+decimal Price = 0;
+decimal quantity = 0;
+bool IsVaild = false;
+
+while (!IsVaild)
+{
+    Console.WriteLine("Enter The Price Of The Product:\n");
+    string PriceEnter = Console.ReadLine();
+    if (decimal.TryParse(PriceEnter, out Price))
+    {
+        IsVaild = true;
+    }
+    else
+    {
+        Console.WriteLine("Invalid Numbers Retry Again\n");
+    }
+
+    IsVaild = false;
+
+    while (!IsVaild)
+    {
+        Console.WriteLine("Enter The Price Of The Product:\n");
+        string quantityEnter = Console.ReadLine();
+        if (decimal.TryParse(quantityEnter, out quantity))
+        {
+            IsVaild = true;
+        }
+        else
+        {
+            Console.WriteLine("Invalid Numbers Retry Again\n");
+        }
+    }
+}
+
+decimal Total = quantity * Price;
+
+Console.WriteLine("///////////////////The Receipt////////////////////\n");
+Console.WriteLine($"The Price ${Price}\n");
+Console.WriteLine($"The Quantity {quantity}\n");
+Console.WriteLine($"The Total Price\n${Total.ToString("f2")}\n");
+
+
+
+
+
