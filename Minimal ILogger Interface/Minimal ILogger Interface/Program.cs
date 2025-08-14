44203fa40c507325db6ba8931262d0900ac6b7c1
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-14 09:15:25 +0300

    Add files via upload

---
+++ Minimal ILogger Interface/Minimal ILogger Interface/Program.cs
@@ -0,0 +1,24 @@
+﻿ILogger NoReferanceObject;
+NoReferanceObject = new FileLogger();
+NoReferanceObject.Log("log message");
+NoReferanceObject = new ConsoleLogger();
+NoReferanceObject.Log(" Console message");
+public class ConsoleLogger : ILogger
+{
+    public void Log(string message)
+    {
+       System.Console.WriteLine(message);
+    }
+}
+public class FileLogger : ILogger
+{
+    public void Log(string message)
+    {
+       System.IO.File.AppendAllText("log.txt", message + "\n");
+    }
+}
+
+public interface ILogger
+{
+    void Log(string message);
+}
