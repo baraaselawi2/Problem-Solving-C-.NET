482215f655f8b16072b21848bd67d41c46c00a8b
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-18 14:31:14 +0300

    Add files via upload

---
+++ Abstract Payment Processor/Abstract Payment Processor/Program.cs
@@ -0,0 +1,35 @@
+﻿List<PaymentProcessor> processors = new List<PaymentProcessor>
+{
+    new CardProcessor(),
+    new CashProcessor()
+};
+foreach (var processor in processors)
+{
+    processor.Process(100);
+}
+
+public abstract class PaymentProcessor {
+
+    public abstract void Process(decimal amount);
+ 
+}
+
+public class CardProcessor : PaymentProcessor
+{
+    public override void Process(decimal amount)
+    {
+        amount = amount * 0.98m;
+        Console.WriteLine($"this with card {amount}");
+    }
+}
+
+
+
+public class CashProcessor : PaymentProcessor
+{
+    public override void Process(decimal amount)
+    {
+     amount = amount * 0.95m;
+        Console.WriteLine($"this with cash {amount}");
+    }
+}
