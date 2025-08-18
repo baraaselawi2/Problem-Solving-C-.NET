7d4c78951da3f13c3bc667dec5a6e6b3841af556
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-18 10:49:13 +0300

    Add files via upload

---
+++ Animal Inheritance Demo/Animal Inheritance Demo/Program.cs
@@ -0,0 +1,47 @@
+﻿List<Animal> animalsound = new List<Animal>
+{
+    new Cat(),
+    new Dog()
+};
+foreach (var item in animalsound)
+{
+    if (item is Cat cat)
+        Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"This cat sound  {item.Speak()}");
+}
+foreach (var item in animalsound)
+{
+    if (item is Dog dog)
+        Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"This Dog  sound  {item.Speak()}");
+}
+
+foreach (var item in animalsound)
+{
+    Console.WriteLine($"THis The Voice of dog and cat {item.Speak()}");
+}
+
+public class Animal
+{
+    public virtual string Speak()
+    {
+        return "hi";
+    }
+}
+
+public class Cat : Animal
+{
+    public override string Speak()
+    {
+        return "Cat voice mew";
+
+    }
+}
+public class Dog : Animal
+{
+    public override string Speak()
+    {
+        return "Dog voice hwhwhwhw ";
+
+    }
+}
