69d7d586f0ef670277b98e672aa8d0175ea00de3
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-18 11:53:47 +0300

    Add files via upload

---
+++ Door Alarm Event/Door Alarm Event/Program.cs
@@ -0,0 +1,12 @@
+﻿var door = new Door();
+door.Opened += () => Console.WriteLine("door alaram");
+door.Open();
+door.Open();
+public class Door
+{
+    public event Action Opened;
+     public void Open()
+    {
+        Opened();
+    }
+}
