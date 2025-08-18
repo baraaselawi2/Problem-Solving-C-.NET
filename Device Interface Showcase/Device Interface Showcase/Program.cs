d320b5560a17a8281c84749f81fa67f6b362fc3a
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-18 12:10:19 +0300

    Add files via upload

---
+++ Device Interface Showcase/Device Interface Showcase/Program.cs
@@ -0,0 +1,40 @@
+﻿var printer =new Printer();
+var scanner = new Scanner();
+
+printer.Start();
+printer.Stop();
+scanner.Start();
+scanner.Stop();
+public interface IDevice {
+
+    void Start();
+    void Stop();
+
+}
+
+public class Printer : IDevice
+{
+    public void Start()
+    {
+        Console.WriteLine("Iam the stert of printert");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Iam the stop of printer");
+    }
+}
+
+public class Scanner : IDevice
+{
+    public void Start()
+    {
+        Console.WriteLine("Iam the stert of Scanner");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Iam the stop pf Scanner");
+      
+    }
+}
