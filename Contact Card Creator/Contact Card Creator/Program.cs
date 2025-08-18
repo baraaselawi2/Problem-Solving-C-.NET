6ecf131923c2791e1568cd5f10b2009f43dd0876
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-18 10:23:34 +0300

    Add files via upload

---
+++ Contact Card Creator/Contact Card Creator/Program.cs
@@ -0,0 +1,22 @@
+﻿var newperson = new person("baera", "baraa.selawi12345@gmail.com", 0782923423);
+Console.WriteLine(newperson.ToString());
+public class person
+{
+    public string name { get; set; }
+    public string Email { get; set; }
+
+    public int phone { get; set; }
+
+    public person(string Name, string email, int Phone)
+    {
+        name = Name;
+        Email = email;
+        phone = Phone;
+
+    }
+
+    public override string ToString()
+    {
+        return $"your name :{this.name} \n your phine number: {this.phone} \n your email  {this.Email} ";
+    }
+}
