22fee96f77a01bb3451a3e58d8fe5c0775bd8c91
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-18 13:43:34 +0300

    Add files via upload

---
+++ Overloaded Calculator/Overloaded Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿var add = new Calculator();
+add.Add(1, 1);
+add.Add(2.2, 9.3);
+add.Add(1, 2, 3, 4);
+public class Calculator
+{
+    public void Add(int x , int y)
+    {
+        Console.WriteLine(x + y);
+    }
+    public void Add(double x , double y)
+    {
+        Console.WriteLine(x + y);
+    }
+    public void Add(params int[] x)
+    {
+        int sum = x.Sum();
+        Console.WriteLine(sum);
+    }
+
+}
