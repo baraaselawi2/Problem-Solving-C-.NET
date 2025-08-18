5c3494fc81c3fbb3325102c06c6726715fffc53e
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-18 14:16:57 +0300

    Add files via upload

---
+++ SmartDoor/SmartDoor/Program.cs
@@ -0,0 +1,25 @@
+﻿var door = new SmarDoor("Main Door");
+var door1 = new SmarDoor("seconde door");
+
+door1.Opened += () => Console.WriteLine($"{door1.DoorName} is opened.");
+door.Closed += () => Console.WriteLine($"{door.DoorName} is closed.");
+door1.Open();
+door.closed();
+public class SmarDoor { 
+
+    public event Action Opened;
+    public event Action Closed;
+    public string DoorName { get; set; }
+    public SmarDoor(string doorname)
+    {
+        DoorName = doorname;
+    }
+    public void Open()
+    {
+        Opened?.Invoke();
+    }
+    public void closed()
+    {
+        Closed?.Invoke();
+    }
+}
