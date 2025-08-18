041577f36a68c63c18d30470bf747328ac9b9df7
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-18 11:40:26 +0300

    Add files via upload

---
+++ Validated Property Example/Validated Property Example/Program.cs
@@ -0,0 +1,27 @@
+﻿var cont = new PropertyExample (200);
+Console.WriteLine(cont.MyProperty);
+
+public class PropertyExample
+{
+	private int age;
+    public bool IsAdult { get; private set; }
+
+    public int MyProperty
+    {
+        get { return age; }
+        set
+        {
+            if (value < 0 || value > 120)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            age = value;
+        }
+    }
+    public PropertyExample(int Age )
+    {
+        MyProperty = Age;
+	
+    }
+
+}
