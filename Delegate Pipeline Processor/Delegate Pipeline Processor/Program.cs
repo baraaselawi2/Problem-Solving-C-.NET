fa17e601a326344ce15a266667a03ea350e6753e
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-19 16:45:44 +0300

    Add files via upload

---
+++ Delegate Pipeline Processor/Delegate Pipeline Processor/Program.cs
@@ -0,0 +1,11 @@
+﻿Func<string, string> upperCase = s => s.ToUpperInvariant();
+Func<string, string> Tirm = s => s.Trim();
+Func<string, string> Timestamp = s => $"{s} - {DateTime.Now:O}";
+var pipeline = new Func<string, string>[] { upperCase, Tirm, Timestamp };
+foreach (var item in pipeline)
+{
+    Console.WriteLine(item("  Hello World  "));
+}
+
+
+
