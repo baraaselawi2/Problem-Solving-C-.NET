a3227a8536b222a44a6e681d1c8d81e8afb7c347
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-19 12:10:34 +0300

    Add files via upload

---
+++ Interfaces vs. Abstract Classes/Interfaces vs. Abstract Classes/Program.cs
@@ -0,0 +1,46 @@
+﻿var shapes = new List<IShape>
+{
+    new Circule { radius = 5 },
+    new Rectangle { width = 4, height = 6 }
+};
+foreach (var shape in shapes)
+{
+    if (shape is Circule circle)
+    {
+        Console.WriteLine($"Circle with radius {circle.radius}");
+    }
+    else if (shape is Rectangle rectangle)
+    {
+        Console.WriteLine($"Rectangle with width {rectangle.width} and height {rectangle.height}");
+    }
+    else
+    {
+        Console.WriteLine("Unknown shape");
+    }
+}
+public abstract class Shape: IShape
+{
+    public abstract double Area();
+}
+interface IShape
+{
+    double Area();
+}
+public class Circule : IShape
+{
+    public double radius { get; set; }
+    const double pi = 3.14;
+    public double Area()
+    {
+        return pi * radius * radius;
+    }
+}
+public class Rectangle : IShape
+{
+    public double width { get; set; }
+    public double height { get; set; }
+    public double Area()
+    {
+        return width * height;
+    }
+}
