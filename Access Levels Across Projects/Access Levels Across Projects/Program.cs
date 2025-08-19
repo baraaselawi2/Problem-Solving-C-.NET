3d27848cf7e6396ea7f5f529dfd6ec5c9c1de8ae
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-19 10:44:50 +0300

    Add files via upload

---
+++ Access Levels Across Projects/Access Levels Across Projects/Program.cs
@@ -0,0 +1,14 @@
+﻿var sumtownumbers = new sumtownumbers(2, 4);
+Console.WriteLine(sumtownumbers.sum);
+internal class sumtownumbers { 
+public int x { get; set; }
+ public int y { get; set; }
+
+    public int sum=> x + y;
+    public sumtownumbers(int X , int Y)
+    {
+        x = Y;
+        y = Y;
+    }
+
+}
