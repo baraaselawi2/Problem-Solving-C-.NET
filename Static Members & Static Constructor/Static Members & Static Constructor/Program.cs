a3743e5817a517e84d9a890472f70cc0a57e3ca2
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-19 12:38:00 +0300

    Add files via upload

---
+++ Static Members & Static Constructor/Static Members & Static Constructor/Program.cs
@@ -0,0 +1,24 @@
+﻿var s1 = new Student();
+var s2 = new Student();
+var s3 = new Student();
+var s4 = new Student();
+
+Console.WriteLine(Student.Count);
+public class Student
+{
+    public static int Count { get; private set; }
+
+  
+    static Student()
+    {
+        Count = 0;
+    }
+
+
+    public Student()
+    {
+        Count++;
+    }
+}
+
+
