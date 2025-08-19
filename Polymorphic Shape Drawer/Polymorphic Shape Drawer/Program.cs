26ee040aeb5749a72ab3de4633f72e25dc388980
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-19 16:28:08 +0300

    Add files via upload

---
+++ Polymorphic Shape Drawer/Polymorphic Shape Drawer/Program.cs
@@ -0,0 +1,39 @@
+﻿List<Shape> shapes = new List<Shape>
+{
+    new Circle(),
+    new Triangle(),
+    new Square()
+};
+foreach (Shape shape in shapes)
+{
+
+    shape.Draw();
+}
+
+public class Shape {
+
+    public virtual void Draw()
+    {
+        Console.WriteLine("Drawing a shape");
+    }
+}
+public class Circle: Shape {
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a circle");
+    }
+}
+public class Triangle : Shape {
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a Triangle");
+    }
+}
+public class Square : Shape {
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a Square");
+    }
+}
+
+
