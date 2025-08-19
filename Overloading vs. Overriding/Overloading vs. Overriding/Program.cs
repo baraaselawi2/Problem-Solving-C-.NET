e6f32e6f6a7218f4945cff73d0a19e822d6dafab
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-19 12:30:32 +0300

    Add files via upload

---
+++ Overloading vs. Overriding/Overloading vs. Overriding/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.Contracts;
+var printer = new Printer();
+var report = new Report();
+var pdfReport = new PdfReport();
+Console.WriteLine(report.Render());
+Console.WriteLine(pdfReport.Render());
+Console.WriteLine(printer.print(10));
+Console.WriteLine(printer.print("Baraa Selawi"));
+
+public class Printer { 
+
+public string print (int print)
+    {
+            return "Printing int: " + print;
+    }
+public string print (string print)
+    {
+            return "Printing int: " + print;
+    }
+
+}
+public class Report {
+
+    virtual public string Render()
+    {
+        return "Printing Report";
+    }
+
+}
+public class PdfReport : Report  { 
+
+   override public string Render()
+    {
+        return "Printing PDF Report";
+    }
+
+}
