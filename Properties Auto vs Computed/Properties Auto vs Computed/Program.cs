11262640bcfc4bda96ddd1aa98acb538b7188f52
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-19 10:13:56 +0300

    Add files via upload

---
+++ Properties Auto vs Computed/Properties Auto vs Computed/Program.cs
@@ -0,0 +1,57 @@
+﻿//var mix = new AutoComputed
+//{
+//    firstname = "Baraa",
+//    lastName = "Baraa"
+//};
+//Console.WriteLine(mix.FullName);
+//public class AutoComputed {
+//    public string firstname { get; set; }
+//    public string lastName { get; set; }
+//    public string FullName {
+//        get
+//        {
+//            string value  = lastName + ", " + firstname;
+
+
+
+//            if (string.IsNullOrEmpty(firstname) || string.IsNullOrEmpty(lastName))
+//            {
+//                throw new InvalidOperationException("First name or last name cannot be null or empty.");
+//            }
+//            else
+//            {
+//                return $"{firstname} {lastName}";
+//            }
+//        }
+
+//}
+
+//}
+
+var mix = new AutoComputed
+{
+	firstname = "Baraa",
+	lastname = "Baraa"
+}; 
+Console.WriteLine(mix.fullname);
+
+public class AutoComputed
+{
+    public string firstname { get; set; }
+    public string lastname { get; set; }
+
+    public string fullname
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(firstname) || string.IsNullOrEmpty(lastname))
+            {
+                throw new InvalidOperationException("First name or last name cannot be null or empty.");
+            }
+            else
+            {
+                return $"{firstname} {lastname}";
+            }
+        }
+    }
+}
