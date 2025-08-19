ca962fee8d4b7e7219fbb6adcfdf9edff0d99f10
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-19 14:43:46 +0300

    Add files via upload

---
+++ Partial Classes/Partial Classes/Program.cs
@@ -0,0 +1,25 @@
+﻿var baraa = new Baraa
+{
+    Name = "Baraa",
+    Age = 25,
+    height = 170
+};
+Console.WriteLine(baraa.GetInfo());
+
+partial class Baraa { 
+
+public int Age { get; set; } 
+    public int height { get; set; } 
+
+    public int sum => Age + height;
+
+}
+partial class Baraa
+{
+    public string Name { get; set; }
+    public string GetInfo()
+    {
+        return $"Name: {Name}, Age: {Age}, Height: {height}, Sum: {sum}";
+    }
+}
+
