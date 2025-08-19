e9e6994b5bea8b2de9c7f1baca46131376c8b328
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-19 11:43:19 +0300

    Add files via upload

---
+++ Overriding object Members (Equality)/Overriding object Members (Equality)/Program.cs
@@ -0,0 +1,25 @@
+﻿var student1 = new Student { StudentId = Guid.NewGuid()};
+student1.ToString();
+student1.Equals(student1);
+student1.GetHashCode();
+Console.WriteLine($"1) To String Methode : \n * {student1.ToString()}");       
+Console.WriteLine($"2) This Equality Of Object : \n * {student1.Equals(student1)}"); 
+Console.WriteLine($"3) This Hash Code : \n * {student1.GetHashCode()}");   
+public class Student { 
+
+public Guid StudentId { get; set; }
+
+    public override string ToString()
+    {
+        return $"Student ID: {StudentId}";
+    }
+    public override bool Equals(object? obj)
+    {
+        return obj is Student student && 
+               StudentId.Equals(student.StudentId);
+    }
+    override public int GetHashCode()
+    {
+      return StudentId.GetHashCode();
+    }   
+}
