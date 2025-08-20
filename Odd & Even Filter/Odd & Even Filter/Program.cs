9ac5a492905719a51b1b786bd175bfa5e84ae723
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-20 15:00:24 +0300

    Add files via upload

---
+++ Odd & Even Filter/Odd & Even Filter/Program.cs
@@ -0,0 +1,9 @@
+﻿List<int> numbers = new List<int> { 1,10,30, 2, 3, 4, 5, 6, 7, 8 };
+
+Func<int, bool> func = x => x % 2 == 0;
+var evens = numbers.Where(func);
+
+foreach (var number in evens)
+{
+    Console.WriteLine(number);
+}
