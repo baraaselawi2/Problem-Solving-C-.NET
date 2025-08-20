d65894f046a10d469414ac23f39f37bdb5c20474
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-20 16:00:43 +0300

    Add files via upload

---
+++ Event-based Collection/Event-based Collection/Program.cs
@@ -0,0 +1,14 @@
+﻿var stock = new Stock { Symbol = 'B' };
+stock.OnPriceChanged += (s) => Console.WriteLine($"Stock {s.Symbol} price changed to {s.Price}");
+stock.pricechanged(100.50m);
+public class Stock {
+    public event Action<Stock> OnPriceChanged;
+    public decimal Price { get; set; }
+   public  char Symbol { get; set; }
+
+    public void pricechanged(decimal price)
+    {
+        this.Price= price;
+        OnPriceChanged?.Invoke(this);
+    }
+}
