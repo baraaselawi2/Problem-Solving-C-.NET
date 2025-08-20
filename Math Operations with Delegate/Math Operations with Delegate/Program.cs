d73db0cb233d96cfe7858d4b57f2f33b7131cc07
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-20 09:40:05 +0300

    Add files via upload

---
+++ Math Operations with Delegate/Math Operations with Delegate/Program.cs
@@ -0,0 +1,11 @@
+﻿List<Func<int, int, int>> operations = new List<Func<int, int, int>>
+{
+    (x, y) => x + y,
+    (x, y) => x - y,
+    (x, y) => x * y,
+    (x, y) => x / y
+};
+foreach (var op in operations)
+{
+    Console.WriteLine(op(5, 10));
+}
