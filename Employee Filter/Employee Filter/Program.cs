fd7a8204415678780b52bbd02b0f40410020a491
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-20 09:26:17 +0300

    Add files via upload

---
+++ Employee Filter/Employee Filter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+List<Employee> employees = new List<Employee>
+{
+    new Employee { Id = 1, Name = "Baraa", Salary = 200 },
+    new Employee { Id = 2, Name = "Selawi", Salary = 300 },
+    new Employee { Id = 4, Name = "Selawi", Salary = 3000 },
+    new Employee { Id = 3, Name = "Adnan", Salary = 700 }
+};
+Func<Employee , bool > filter= s => s.Salary > 500;
+
+foreach (var employee in employees)
+{
+    if (filter(employee))
+    {
+        Console.WriteLine($"Id: {employee.Id}, Name: {employee.Name}, Salary: {employee.Salary}");
+    }
+}
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Salary { get; set; }
+}
