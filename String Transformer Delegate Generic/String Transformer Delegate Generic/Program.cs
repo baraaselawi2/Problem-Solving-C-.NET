800b052ce070e713a10e4c3d1afacfa860899bd1
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-20 10:13:38 +0300

    Add files via upload

---
+++ String Transformer Delegate Generic/String Transformer Delegate Generic/Program.cs
@@ -0,0 +1,11 @@
+﻿List<string> strings = new List<string>
+{
+    "Baraa",
+    "Selawi",
+    "test"
+};
+Func<string, string> transformer = name => name.ToLower();
+Action<string> Print = name => Console.WriteLine(name);
+
+strings.Select(transformer).ToList().ForEach(Print);
+
