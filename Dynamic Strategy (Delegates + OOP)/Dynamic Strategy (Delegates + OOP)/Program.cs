5750184bcc86415ab83b83b93e6afdda1e436fde
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-20 15:40:17 +0300

    Add files via upload

---
+++ Dynamic Strategy (Delegates + OOP)/Dynamic Strategy (Delegates + OOP)/Program.cs
@@ -0,0 +1,15 @@
+﻿var Calculator = new calculator();
+
+Console.WriteLine(Calculator.Compute(4, 3, (x, y) => x + y)); 
+Console.WriteLine(Calculator.Compute(5, 3, (x, y) => x - y));
+Console.WriteLine(Calculator.Compute(5, 3, (x, y) => x * y)); 
+Console.WriteLine(Calculator.Compute(5, 3, (x, y) => x / y)); 
+
+
+public class calculator
+{
+    public int Compute(int x, int y, Func<int, int, int> operation)
+    {
+                return operation(x, y);
+    }
+}
