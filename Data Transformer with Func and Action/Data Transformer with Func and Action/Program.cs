526f505871e7e214c755278eb9f1fdfaaa3b7e65
baraaselawi2 <163642964+baraaselawi2@users.noreply.github.com>
2025-08-20 09:11:01 +0300

    Add files via upload

---
+++ Data Transformer with Func and Action/Data Transformer with Func and Action/Program.cs
@@ -0,0 +1,4 @@
+﻿List<int> ints = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+Func<int, int> transform = x => x * x;
+Action<int> logger = x => Console.WriteLine($"Result = {x}");
+ints.Select(transform).ToList().ForEach(logger);
